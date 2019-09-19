7ef71d0b888464ffa199b1edefb29ebb02c79764
Amgad Malik <amgadmalik@yahoo.com>
2019-09-19 01:38:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array.join
+     
+  elsif array.length == 2 
+    array.join(" and ")
+  
+  else
+    last_element = "and #{array[-1]}"
+    array.pop
+    array << last_element
+    array.join(", ")
+  end
 end
