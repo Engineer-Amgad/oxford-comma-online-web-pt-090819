f28af6e9c324589daeef7a80dbac3a7de1d9945a
Amgad Malik <amgadmalik@yahoo.com>
2019-09-19 01:33:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
-   badge_array = []
+  if array.length == l 
+    return
+     
+  elsif array.length == 2 
+    array.join(",")
   
   else
     last_element = "and, #{array[-1]}"
